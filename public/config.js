bc5ef490d4ba0bb2e972b1b03c1c4b2ce2a754b1
Mario Reder <mario.reder@pm.me>
2025-10-09 09:06:36 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "how does this work",
   "VITE_SEO_SITE_DESCRIPTION": "why did my son even buy me this thing",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/how-does-this-work-1964/",
+  "VITE_SEO_SITE_URL": "https://tojimmyhopeyouhaveagoodbirthdayfrompa.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
